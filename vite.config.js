a6980edf38d030113c6b66fdbfd5ec18275ca65d
Jesus Laso <jesuslaso94@gmail.com>
2025-03-19 19:44:20 +0100

    Añadida la configuracion multipagina en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: '/modern_art_gallery/', 
+    build: {
+        rollupOptions: {
+          input: {
+            index: resolve(__dirname, 'index.html'),
+            second: resolve(__dirname, 'location.html')
+          }
+        }
+      }
 });
+
+
